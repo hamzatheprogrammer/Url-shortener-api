0ff99afc4ad99775438b44a8378d1f7b0ea8d86e
Hamza saleem <rydergamer1122@gmail.com>
2024-07-25 20:08:33 +0500

    mdified
    modified on 25-7-24

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express()
+require("dotenv").config()
+const shortUrl = require("node-url-shortener")
+
+
+const port = process.env.PORT || 3000
+
+app.get("/:url", (req,res)=>{
+    const url = req.params.url
+    shortUrl.short(url, function (err, url) {
+        if (err){
+            res.json({
+                status: 500,
+                developer: "hamza-saleem",
+                message: "err while shortening url" 
+
+
+            })
+        }
+        else{
+            res.json({
+                status: 200,
+                developer: "hamza-saleem",
+                url: url
+            })
+        }
+    });
+    
+    
+})
+
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
